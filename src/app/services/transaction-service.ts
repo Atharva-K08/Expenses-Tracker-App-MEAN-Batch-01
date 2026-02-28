6cce96e959236630c1e20b7341d0cdc41b46ba12
Atharva Kumbhar <111524252+Atharva-K08@users.noreply.github.com>
2026-02-28 11:16:46 +0530

    transaction-form-integration

---
+++ src/app/services/transaction-service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  apiURL = 'http://localhost:5000/api';
+  token = localStorage.getItem('token') || '';
+
+  constructor(private http: HttpClient) {}
+
+  addCategory(category: any) {
+    return this.http.post(`${this.apiURL}/transcation-category/`, category, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+  }
+  getCategory() {
+    return this.http.get(`${this.apiURL}/transcation-category/`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+  }
+  addTransaction(transaction: any) {
+    return this.http.post(`${this.apiURL}/transaction/`, transaction, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+  }
+}
--- src/pages/transaction-page/transaction-page.ts
+++ src/pages/transaction-page/transaction-page.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TransactionService } from '../../app/services/transaction-service';
 @Component({
   selector: 'app-transaction-page',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './transaction-page.html',
   styleUrl: './transaction-page.css',
 })
-export class TransactionPage {
-
+export class TransactionPage implements OnInit {
+  categories: any = [];
+  categoryForm!: FormGroup;
+  transactionForm!: FormGroup;
+  constructor(
+    private transactionService: TransactionService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+  ngOnInit(): void {
+    this.transactionService.getCategory().subscribe((res: any) => {
+      this.categories = res.data;
+      this.cdr.detectChanges();
+    });
+    this.categoryForm = new FormGroup({
+      name: new FormControl(''),
+    });
+    this.transactionForm = new FormGroup({
+      type: new FormControl(''),
+      currency: new FormControl(''),
+      amount: new FormControl(''),
+      description: new FormControl(''),
+      method: new FormControl(''),
+      category: new FormControl(''),
+      date: new FormControl(''),
+      status: new FormControl(''),
+    });
+  }
+  submitCategory = () => {
+    if (this.categoryForm.valid) {
+      console.log('category submit: ', this.categoryForm.value);
+      this.transactionService.addCategory(this.categoryForm.value).subscribe((res) => {
+        console.log('category added: ', res);
+        this.categoryForm.reset();
+        this.transactionService.getCategory().subscribe((res: any) => {
+          this.categories = res.data;
+          this.cdr.detectChanges();
+        });
+      });
+    }
+  };
+  submitTransaction = () => {
+    if (this.transactionForm.valid) {
+      console.log('transaction submit: ', this.transactionForm.value);
+      this.transactionService.addTransaction(this.transactionForm.value).subscribe((res) => {
+        console.log('transaction added: ', res);
+        this.transactionForm.reset();
+      });
+    }
+  };
 }
